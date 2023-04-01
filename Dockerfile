919971ac711c2d6e6d4e29c293f71586605a5427
Raphael Beekmann <raphael.beekmann@gmail.com>
2023-04-01 15:32:12 +0200

    build: adapt server to deployement

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,3 @@
-### STAGE 1 BUILD THE APP ###
-# specify the node base image with your desired version node:<version>
 FROM node:16
 RUN mkdir /MesseMap
 WORKDIR /MesseMap
@@ -7,6 +5,3 @@ COPY package.json .
 RUN npm install --quiet
 COPY . .
 RUN npm run build
-
-### STAGE 2 RUN THE APP ###
-
--- Server.js
+++ Server.js
@@ -3,7 +3,7 @@ const proxy = require('html2canvas-proxy');
 const path = require('path');
 // App and preferences
 const app = express();
-const port = 1337;
+const port = 8091;
 // url definitions
 app.use('/assets', express.static('assets')); // Serve static files
 app.use('/proxy', proxy()); // Proxify external assets for html2canvas
