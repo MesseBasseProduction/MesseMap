d79ed213fbeb2525009e6c28980081ca97c50816
ArthurBeaulieu <peso@abeaulieu.fr>
2023-11-16 16:42:29 +0100

    fix tough-cookie proto pollution by re-implementing proxy into project instead of npm pkg

--- src/js/Server.js
+++ src/js/Server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const proxy = require('html2canvas-proxy');
 const path = require('path');
+const url = require('url');
 const fs = require('fs');
+const cors = require('cors');
+const request = require('request');
 const compression = require('compression');
 const zlib = require('node:zlib');
 // App and preferences
@@ -9,6 +11,35 @@ const version = '1.0.3';
 const port = 8010;
 const exportPath = path.join(__dirname, '../../saved'); // Must match Dockerfile value
 const app = express();
+// Proxy abstraction, taken from html2canvas-proxy-nodejs, but imported here to avoid sec issues w/ npm
+const proxy = () => {
+  function validUrl(req, res, next) {
+    if (!req.query.url) {
+      next(new Error('No url specified'));
+    } else if (typeof req.query.url !== 'string' || url.parse(req.query.url).host === null) {
+      next(new Error(`Invalid url specified: ${req.query.url}`));
+    } else {
+      next();
+    }
+  }
+  const app = express.Router();
+  app.get('/', cors(), validUrl, (req, res, next) => {
+    switch (req.query.responseType) {
+      case 'blob':
+        req.pipe(request(req.query.url).on('error', next)).pipe(res);
+        break;
+      case 'text':
+      default:
+        request({ url: req.query.url, encoding: 'binary' }, (error, response, body) => {
+          if (error) {
+            return next(error);
+          }
+          res.send(`data:${response.headers['content-type']};base64,${Buffer.from(body, 'binary').toString('base64')}`);
+        });
+    }
+  });
+  return app;
+};
 // Log
 console.log(`${(new Date()).toISOString()} | MesseMap v${version} | Starting server and proxy`);
 // Ensure responses are compressed through this midleware
@@ -26,7 +57,7 @@ app.use('/proxy', proxy()); // Proxify external assets for html2canvas, avoid ca
 // Serve main html at /
 app.get('/', (req, res) => {
   console.log(`${(new Date()).toISOString()} | MesseMap v${version} | index.html page requested`);
-  res.sendFile(path.join(__dirname, '../../assets/html/index.html'));  
+  res.sendFile(path.join(__dirname, '../../assets/html/index.html'));
 });
 // Serve documentation html at /about
 /* Scheduled for next release
